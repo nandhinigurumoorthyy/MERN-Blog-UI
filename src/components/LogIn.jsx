4f1ccaa5cfb9649403e41fd25dd275a841ec187b
Nandhini <nandhinigurumoorthy2003@gmail.com>
2025-05-18 18:44:40 +0530

    commit

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -186,7 +186,7 @@ const LogIn = () => {
 
                     <div className="flex items-center justify-center pt-5">
                         <button type="submit"
-                            className="text-xl w-32 h-12 rounded bg-[#3434a7] text-white relative overflow-hidden group z-10 hover:text-white duration-1000"
+                            className="cursor-pointer text-xl w-32 h-12 rounded bg-[#3434a7] text-white relative overflow-hidden group z-10 hover:text-white duration-1000"
                         >
                             <span
                                 className="absolute bg-[#3a3aaa] w-36 h-36 rounded-full group-hover:scale-100 scale-0 -z-10 -left-2 -top-10 group-hover:duration-500 duration-700 origin-center transform transition-all"
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -184,7 +184,7 @@ function SignUp() {
             <div className="flex items-center justify-center pt-5">
               <button
                 type="submit"
-                className="text-xl w-32 h-12 rounded bg-[#3434a7] text-white relative overflow-hidden group z-10 hover:text-white duration-1000"
+                className="cursor-pointer text-xl w-32 h-12 rounded bg-[#3434a7] text-white relative overflow-hidden group z-10 hover:text-white duration-1000"
               >
                 <span className="absolute bg-[#3a3aaa] w-36 h-36 rounded-full group-hover:scale-100 scale-0 -z-10 -left-2 -top-10 group-hover:duration-500 duration-700 origin-center transform transition-all"></span>
                 <span className="absolute bg-[#2e2ea3] w-36 h-36 -left-2 -top-10 rounded-full group-hover:scale-100 scale-0 -z-10 group-hover:duration-700 duration-500 origin-center transform transition-all"></span>
